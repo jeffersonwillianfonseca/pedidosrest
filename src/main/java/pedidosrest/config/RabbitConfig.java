9c1456ea293ee7830da188d1e9a3d0e39b82f8b4
JeffersonWillian <jefferson.has.fonseca@gmail.com>
2019-01-28 22:25:29 -0200

    Modifica config do contaynerFactory para utilizar a  arquivo de configuracao

--- src/main/java/pedidosrest/config/RabbitConfig.java
+++ src/main/java/pedidosrest/config/RabbitConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,12 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class RabbitConfig {
 
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
     @Bean
     public ConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory("localhost");
+        return new CachingConnectionFactory(host);
     }
 
     @Bean
